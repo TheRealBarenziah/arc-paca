aa3fd7dff5c34b80b876b39f83f447b3cc0e0ed1
samhain oh <samhainoh@gmail.com>
2019-06-27 23:25:13 +0200

    finalize resourcePage/ resourceCard

--- src/components/ResourcesDisplay/ResourceCard.jsx
+++ src/components/ResourcesDisplay/ResourceCard.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './ResourceCard.scss';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
-export default function ResourceCard({title, description, labels, url}) {
+export default function ResourceCard({ title, description, labels, url }) {
   return (
-    <div className="containerResourceCard">
-      <Container>
-      {labels.map((label) => {
-        return <div className="label">{label}</div>
-      })}
-      <h3>{title} </h3>
-      <a href={url} alt={title}>My Link</a>
-      <p className="description">{description}</p>
-     
+    <div>
+      <Container className="containerResourceCard">
+        <Row className="test1">
+          {labels.map((label) => {
+            return (
+              <Col className="colLabel">
+                <p className="label">{label}</p>
+              </Col>
+            )
+          })}
+
+        </Row>
+        <h3>{title} </h3>
+        <p className="description">{description}</p>
+        <a href={url} alt={title}>My Link</a>
+
       </Container>
     </div>
   )
