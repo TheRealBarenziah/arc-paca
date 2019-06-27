f5bf9749905da9f6883f7d4f9abbeeb2bd0690c0
Dan CARBONE <dan.carbone@live.fr>
2019-06-27 14:10:34 +0200

    Add Bootstrap

--- src/components/Citizen/Citizen.jsx
+++ src/components/Citizen/Citizen.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import { Button, Container, Col, Row} from 'reactstrap';
+
 import './Citizen.scss';
 
 export default class Citizen extends Component {
   render() {
     return (
       <div className='containerCitizen'>
-        <p>Hi! You're in Citizen Component!</p>
+        <Container>
+          <Row>
+            <Col>
+              <p>Hi! You're in Citizen Component!</p>
+              <Button color="danger">Danger!</Button> 
+            </Col>
+          </Row>
+        </Container>
       </div>
+
     );
   }
 }
